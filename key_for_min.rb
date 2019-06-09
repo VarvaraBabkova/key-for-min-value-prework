bb0d9994c574acb3283207e70b1a10748d5a963c
Varvara Babkova <varvara.munday@gmail.com>
2019-06-09 13:18:02 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,5 +3,14 @@
 
 def key_for_min_value(name_hash)
   return nil if name_hash == {}
-    
+  min_price = name_hash.first.value
+  min_name = name_hash.first.key
+
+  name_hash.collect do |name, price|
+    if price < min_price
+      min_price = price
+      min_name = name
+    end
+  end
+  return name
 end
